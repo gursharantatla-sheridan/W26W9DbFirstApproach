d83a350f9672f75558e33935d3405123cd15695d
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-03-10 19:34:04 -0400

    complete

--- W26W9DbFirstApproach/MainWindow.xaml.cs
+++ W26W9DbFirstApproach/MainWindow.xaml.cs
@@ -25,17 +25,103 @@ namespace W26W9DbFirstApproach
         public MainWindow()
         {
             InitializeComponent();
+            LoadStandards();
         }
 
         private void LoadStudents()
         {
-            var students = db.Students.ToList();
+            //var students = db.Students.ToList();
+            //var students = (from s in db.Students
+            //               select new { s.StudentId, s.StudentName, s.Standard.StandardName }).ToList();
+
+            var students = db.Students
+                             .Select(s => new { s.StudentId, s.StudentName, s.Standard.StandardName })
+                             .ToList();
+
             grdStudents.ItemsSource = students;
+        }
+
+        private void LoadStandards()
+        {
+            var standards = db.Standards.ToList();
+            cmbStandard.ItemsSource = standards;
+            cmbStandard.DisplayMemberPath = "StandardName";
+            cmbStandard.SelectedValuePath = "StandardId";
         }
 
         private void btnLoadStudents_Click(object sender, RoutedEventArgs e)
         {
             LoadStudents();
+        }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            var std = db.Students.Find(id);
+
+            if (std != null)
+            {
+                txtName.Text = std.StudentName;
+                cmbStandard.SelectedValue = std.StandardId;
+            }
+            else
+            {
+                txtName.Text = "";
+                cmbStandard.SelectedIndex = -1;
+                MessageBox.Show("Invalid ID. Please try again");
+            }
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            Student std = new Student();
+            std.StudentName = txtName.Text;
+            std.StandardId = (int)cmbStandard.SelectedValue;
+
+            db.Students.Add(std);
+            db.SaveChanges();
+
+            LoadStudents();
+            MessageBox.Show("New student added");
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            var std = db.Students.Find(id);
+
+            std.StudentName = txtName.Text;
+            std.StandardId = (int)cmbStandard.SelectedValue;
+
+            db.SaveChanges();
+
+            LoadStudents();
+            MessageBox.Show("Student updated");
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            var std = db.Students.Find(id);
+
+            db.Students.Remove(std);
+            db.SaveChanges();
+
+            LoadStudents();
+            MessageBox.Show("Student deleted");
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            // query syntax
+            //var students = (from s in db.Students
+            //               where s.StudentName.Contains(txtName.Text)
+            //               select s).ToList();
+
+            // method syntax
+            var students = db.Students.Where(s => s.StudentName.Contains(txtName.Text)).ToList();
+
+            grdStudents.ItemsSource = students;
         }
     }
 }
